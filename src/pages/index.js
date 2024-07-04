a70a3b9363a2d7277ed4f46a1f391ef877bae10c
Saman Tofighian <samantofighian78@gamil.com>
2024-07-05 01:48:33 +0430

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,3 @@
-import MainMenu from "../../Components/header/mainMenu/MainMenu";
-import Navbar from "../../Components/header/navbar/Navbar";
-
 export default function Home() {
   return <></>;
 }
